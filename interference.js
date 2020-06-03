b886146081d724fb0aabb02f849888e1eb7ff624
unknown <anacarolinaosantos85@gmail.com>
2020-06-03 12:55:32 +0300

    first commit

---
+++ interference.js
@@ -0,0 +1,18 @@
+function showLog(message) {
+    /*showLog(message: any) passo qualquer
+    tipo abaixo */
+    console.log(message);
+}
+showLog("hello world");
+showLog(171);
+/*string
+number
+boolean
+arrays
+classe*/
+var test = ["Hello World", "Test"];
+/*String[] nomes;* assim seria a declarção de
+array no java*/
+var isEnabled = true;
+var test2 = ['Hello', 'Test'];
+//# sourceMappingURL=interference.js.map 
---
+++ interference.ts
@@ -0,0 +1,17 @@
+function showLog(message:any) {
+    /*showLog(message: any) passo qualquer 
+    tipo abaixo */
+    console.log(message);
+}
+showLog("hello world");
+showLog(171);
+/*string
+number
+boolean
+arrays
+classe*/
+let test: Array<string> = ["Hello World", "Test"];
+/*String[] nomes;* assim seria a declarção de 
+array no java*/
+let isEnabled: boolean = true;
+/*let test2: [string] = ['Hello', 'Test'];*/
